591f1eab974bb6b92f0e5b6921f7e71425fe5f7f
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2022-02-19 11:05:09 -0500

    Created form

--- src/Search.js
+++ src/Search.js
@@ -2,19 +2,18 @@ import React from "react";
 import axios from "axios";
 
 export default function Search() {
-function handleResponse(response) {
-  alert(`The weather in Charlotte is ${response.data.main.temp}`);
-}
+
 
 
   let apiKey = "795212133d0fdb468c01b067f3e90e38";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(handleResponse);
-
   return (
     <div className="Search">
-      Hello from Weather
+      <form>
+        <input type="search" placeholder="Enter a city..." autoFocus={true}/>
+        <input type="submit" value="Search" />
+      </form>
     </div>
   )
 }
