a350d49343befd30b6486d0d4c1568c5f4c9cad8
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2022-03-12 15:56:17 -0500

    Added basic build with header and footer with links to GitHub Repository

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+  <div className="App">
+  
+    <h1>Weather Application</h1>
+
+    <footer>
+    This project was coded by <a href="https://www.linkedin.com/in/chrissy-mickler-23104427/" target="_blank" rel="noreferrer">Chrissy Mickler</a> and is{" "} 
+      <a href="https://github.com/cmickler17/weather-app-react" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
+    </footer>
+
+  </div>
+  )
 }
