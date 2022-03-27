dac05cfed8e24eb08b8a25e8d14ded1c2cc871ee
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2022-03-27 16:30:49 -0400

    Created the Celsius unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemp from "./WeatherTemp";
 
 export default function WeatherInfo(props) {
     return (
@@ -19,8 +20,7 @@ export default function WeatherInfo(props) {
                                 </div>
         
                                <div className="float-left">
-                                <span className="temperature">{Math.round(props.data.temperature)}</span>
-                                <span className="unit">°F</span>
+                                   <WeatherTemp fahrenheit={props.data.temperature} />
                                 </div>
                             </div>
                     </div>
