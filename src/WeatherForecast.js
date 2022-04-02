7ef159be3410baad1979ae21fd44df9a6ba4dfea
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2022-04-02 19:17:05 -0400

    Built 1 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+    const [loaded, setLoaded] = useState(false);
+    const [forecast, setForecast] = useState(null);
+
     function handleResponse (response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
     }
 
+    if (loaded) {
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    <div className="col">
+                       <WeatherForecastDay data={forecast[0]} />
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
     let apiKey = "795212133d0fdb468c01b067f3e90e38";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(handleResponse);
-  
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">Thu</div>
-                    <WeatherIcon code="01d" size={36} />
-                    <div className="WeatherForecast-temps">
-                        <span className="WeatherForecast-temp-max">54°</span>
-                        <span className="WeatherForecast-temp-min">40°</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+    }
 }
+
+
+    
+  
+  
