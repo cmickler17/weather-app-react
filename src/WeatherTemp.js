ce165c1b0d2a71981c08871169f7296012a146cc
cmickler17 <87788655+cmickler17@users.noreply.github.com>
2022-04-03 10:10:36 -0400

    Removed celsius temp option
    Fahrenheit used in North America/US

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,34 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-    const [unit, setUnit] = useState("fahrenheit");
-    function showCelsius(event) {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-
-    function showFahrenheit(event) {
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-
-    function celsius() {
-        return (props.fahrenheit - 32) * 5/9;
-    }
-
-    if (unit === "fahrenheit"){
         return (
             <div className="WeatherTemp">
             <span className="temperature">{Math.round(props.fahrenheit)}</span>
-            <span className="unit"> °F | <a href="/" onClick={showCelsius}>°C</a> </span>
+            <span className="unit"> °F </span>
             </div>
-        )
-    } else {
-        return (
-            <div className="WeatherTemp">
-                <span className="temperature">{Math.round(celsius())}</span>
-                <span className="unit"><a href="/" onClick={showFahrenheit}>°F</a> | °C</span>
-            </div>
-        )
+        );
     }
-}
